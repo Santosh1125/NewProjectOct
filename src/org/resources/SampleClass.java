97221fa9eee38cf194144063a722b98ffdee2e27
Santosh1125 <santoshkumarp1126@gmail.com>
2021-10-08 19:28:29 +0530

    password functionality

--- src/org/resources/SampleClass.java
+++ src/org/resources/SampleClass.java
@@ -2,11 +2,11 @@ package org.resources;
 
 public class SampleClass {
 
-	public static void userName() {
-		System.out.println("username");
+	public static void password() {
+		System.out.println("password");
 	}
 	public static void main(String[] args) {
-		userName();		
-	}
+		password();	
+	}	
 	
 }

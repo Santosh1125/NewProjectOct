f90c69bb8ef30face80f62c9dafec34189fd5ea2
Santosh1125 <92080064+Santosh1125@users.noreply.github.com>
2021-10-08 12:02:25 +0530

    Revert "username functionality"

--- src/org/resources/SampleClass.java
+++ src/org/resources/SampleClass.java
@@ -2,11 +2,5 @@ package org.resources;
 
 public class SampleClass {
 
-	public static void userName() {
-		System.out.println("username");
-	}
-	public static void main(String[] args) {
-		userName();		
-	}
 	
 }
